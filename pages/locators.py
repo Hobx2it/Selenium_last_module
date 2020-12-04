90fb773ae30a77af808a6283bdd78390762df9e2
Аманов Руслан Арсланович <AmanovRA@mcxxi.local>
2020-12-04 15:39:16 +0300

    Убрал неверные аргументы на страницах main_page и login_page, так как идет распаковка:

--- pages/locators.py
+++ pages/locators.py
@@ -8,7 +8,7 @@ class LoginPageLocators():
     ENTER_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
     ENTER_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
     
-    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_PASSWORD_DUPLICATE = (By.CSS_SELECTOR, "#id_registration-password2")
     
--- pages/login_page.py
+++ pages/login_page.py
@@ -15,11 +15,11 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.ENTER_EMAIL), "login_form email_field is not presented"
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.ENTER_PASSWORD), "login_form password_field is not presented"
+        assert self.is_element_present(*LoginPageLocators.ENTER_EMAIL), "login_form email_field is not presented"
+        assert self.is_element_present(*LoginPageLocators.ENTER_PASSWORD), "login_form password_field is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.REGISTRATION_EMAIL), "registration_form email_field is not presented"
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.REGISTRATION_PASSWORD), "registration_form password_field is not presented"
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.REGISTRATION_PASSWORD_DUPLICATE), "registration_form password_reenter_field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "registration_form email_field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD), "registration_form password_field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_DUPLICATE), "registration_form password_reenter_field is not presented"
